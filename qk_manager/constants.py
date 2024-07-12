43a046b90dab973c9486914d9e69c2c4263a5738
kenya-sk <30319295+kenya-sk@users.noreply.github.com>
2024-07-12 20:07:42 +0900

    (add): save db data on local

--- qk_manager/constants.py
+++ qk_manager/constants.py
@@ -4,3 +4,5 @@ MODULE_HOME: Path = Path(__file__).resolve().parents[1]
 MODULE_SRC: Path = Path(__file__).resolve().parents[0]
 
 DEFAULT_EXP_DIRC: Path = MODULE_HOME / "experiments"
+DEFAULT_EXP_DB_FILE: Path = Path("experiment.csv")
+DEFAULT_RUN_DB_FILE: Path = Path("runs.csv")
