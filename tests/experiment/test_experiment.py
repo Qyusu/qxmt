5543250655a6982c2f8df3ace20211312bfcd93e
kenya-sk <30319295+kenya-sk@users.noreply.github.com>
2024-07-24 02:06:17 +0900

    (add): function of loading past experiment

--- tests/experiment/test_experiment.py
+++ tests/experiment/test_experiment.py
@@ -1,22 +1,33 @@
+import json
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
+import pytest
 from pandas.testing import assert_frame_equal
 
 from qk_manager import Experiment
 from qk_manager.constants import DEFAULT_EXP_DB_FILE
+from qk_manager.exceptions import ExperimentNotInitializedError
 
 
 class TestExperimentSettings:
     def test_default(self, tmp_path: Path) -> None:
-        default_exp = Experiment(root_experiment_dirc=tmp_path)
+        default_exp = Experiment(root_experiment_dirc=tmp_path).init()
         assert isinstance(default_exp.name, str)
         assert default_exp.desc == ""
         assert default_exp.current_run_id == 0
         assert default_exp.experiment_dirc.exists()
 
+    def test__is_initialized(self, base_experiment: Experiment) -> None:
+        with pytest.raises(ExperimentNotInitializedError):
+            base_experiment._is_initialized()
+
+        base_experiment.init()
+        assert base_experiment._is_initialized
+
     def test_initialization(self, base_experiment: Experiment) -> None:
+        base_experiment.init()
         assert base_experiment.name == "test_exp"
         assert base_experiment.desc == "test experiment"
         assert base_experiment.current_run_id == 0
@@ -24,32 +35,78 @@ class TestExperimentSettings:
 
 
 class TestExperimentInit:
-    def test__init_db(self, tmp_path: Path) -> None:
-        exp = Experiment(
-            name="valid_test_exp",
-            desc="valid test experiment",
-            root_experiment_dirc=tmp_path,
-        )
-        exp_db = exp.exp_db
+    def test_init(self, base_experiment: Experiment, tmp_path: Path) -> None:
+        assert base_experiment.exp_db is None
 
-        assert exp_db.name == "valid_test_exp"
-        assert exp_db.desc == "valid test experiment"
-        assert exp_db.experiment_dirc == tmp_path / "valid_test_exp"
+        base_experiment.init()
+        exp_db = base_experiment.exp_db
+        assert exp_db.name == "test_exp"  # type: ignore
+        assert exp_db.desc == "test experiment"  # type: ignore
+        assert exp_db.experiment_dirc == tmp_path / "test_exp"  # type: ignore
+
+
+class TestLoadExperiment:
+    def set_dummy_experiment_data(self, experiment_dirc: Path) -> Path:
+        dummy_experiment_dict = {
+            "name": "load_exp",
+            "desc": "load experiment",
+            "experiment_dirc": str(experiment_dirc / "load_exp"),
+            "runs": [
+                {
+                    "run_id": 1,
+                    "desc": "",
+                    "evaluation": {
+                        "accuracy": 0.65,
+                        "precision": 0.69,
+                        "recall": 0.65,
+                        "f1_score": 0.67,
+                    },
+                },
+                {
+                    "run_id": 2,
+                    "desc": "",
+                    "evaluation": {
+                        "accuracy": 0.5,
+                        "precision": 0.47,
+                        "recall": 0.55,
+                        "f1_score": 0.50,
+                    },
+                },
+            ],
+        }
+        exp_json_path = Path(dummy_experiment_dict["experiment_dirc"]) / "experiment.json"
+        exp_json_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(exp_json_path, "w") as json_file:
+            json.dump(dummy_experiment_dict, json_file, indent=4)
+
+        return exp_json_path
+
+    def test_load_experiment(self, base_experiment: Experiment, tmp_path: Path) -> None:
+        exp_json_path = self.set_dummy_experiment_data(tmp_path)
+        base_experiment.load_experiment(exp_json_path)
+        assert base_experiment.name == "load_exp"
+        assert base_experiment.desc == "load experiment"
+        assert base_experiment.experiment_dirc == tmp_path / "load_exp"
+        assert base_experiment.current_run_id == 2
+        assert len(base_experiment.exp_db.runs) == 2  # type: ignore
 
 
 class TestExperimentRun:
     def test_run(self, base_experiment: Experiment) -> None:
         assert base_experiment.current_run_id == 0
-        assert base_experiment.exp_db.runs == []
+        assert base_experiment.exp_db is None
+        with pytest.raises(ExperimentNotInitializedError):
+            base_experiment.run()
 
+        base_experiment.init()
         base_experiment.run()
         assert base_experiment.current_run_id == 1
-        assert len(base_experiment.exp_db.runs) == 1
+        assert len(base_experiment.exp_db.runs) == 1  # type: ignore
 
         base_experiment.run()
         base_experiment.run()
         assert base_experiment.current_run_id == 3
-        assert len(base_experiment.exp_db.runs) == 3
+        assert len(base_experiment.exp_db.runs) == 3  # type: ignore
 
     def test__run_evaluation(self, base_experiment: Experiment) -> None:
         actual = np.array([0, 1, 1, 0, 1])
@@ -80,6 +137,7 @@ class TestExperimentResults:
     #     assert_frame_equal(df, expected_df)
 
     def test_save_experiment(self, base_experiment: Experiment) -> None:
+        base_experiment.init()
         base_experiment.run()
         base_experiment.run()
         base_experiment.save_experiment()
