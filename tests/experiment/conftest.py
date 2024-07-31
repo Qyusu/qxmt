c73a0e30639e8fdc9529a24c3f5746f180158578
kenya-sk <30319295+kenya-sk@users.noreply.github.com>
2024-07-31 18:40:28 +0900

    (add): kernel defined by feature map func

--- tests/experiment/conftest.py
+++ tests/experiment/conftest.py
@@ -7,6 +7,7 @@ import pytest
 
 from qxmt import Experiment
 from qxmt.datasets.schema import Dataset
+from qxmt.feature_maps.base import BaseFeatureMap
 from qxmt.kernels.base import BaseKernel
 from qxmt.models.base import BaseModel
 from qxmt.models.qsvm import QSVM
@@ -14,9 +15,13 @@ from qxmt.models.qsvm import QSVM
 DEVICE = qml.device("default.qubit", wires=2)
 
 
+def empty_feature_map(x: np.ndarray) -> None:
+    qml.Identity(wires=0)
+
+
 class TestKernel(BaseKernel):
-    def __init__(self, device: qml.Device) -> None:
-        super().__init__(device)
+    def __init__(self, device: qml.Device, feature_map: Callable[[np.ndarray], None]) -> None:
+        super().__init__(device, feature_map)
 
     def compute(self, x1: np.ndarray, x2: np.ndarray) -> float:
         return np.dot(x1, x2)
@@ -33,7 +38,7 @@ def base_experiment(tmp_path: Path) -> Experiment:
 
 @pytest.fixture(scope="function")
 def base_model() -> BaseModel:
-    kernel = TestKernel(device=DEVICE)
+    kernel = TestKernel(device=DEVICE, feature_map=empty_feature_map)
     return QSVM(kernel=kernel)
 
 
