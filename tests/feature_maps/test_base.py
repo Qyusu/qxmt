bef43f89cdc482c3e95bec1aa42940d0fb3b1a24
kenya-sk <30319295+kenya-sk@users.noreply.github.com>
2024-08-16 15:17:56 +0900

    (change): feature map test case

--- tests/feature_maps/test_base.py
+++ tests/feature_maps/test_base.py
@@ -6,20 +6,22 @@ from qxmt.exceptions import InputShapeError
 from qxmt.feature_maps.base import BaseFeatureMap
 
 
+class EmptyFeatureMap(BaseFeatureMap):
+    def __init__(self, platform: str, n_qubits: int) -> None:
+        super().__init__(platform, n_qubits)
+
+    def feature_map(self, x: np.ndarray) -> None:
+        qml.Identity(wires=0)
+
+
 @pytest.fixture(scope="function")
 def base_feature_map() -> BaseFeatureMap:
-    class EmptyFeatureMap(BaseFeatureMap):
-        def __init__(self, n_qubits: int) -> None:
-            super().__init__(n_qubits)
-
-        def feature_map(self, x: np.ndarray) -> None:
-            qml.Identity(wires=0)
-
-    return EmptyFeatureMap(n_qubits=2)
+    return EmptyFeatureMap(platform="pennylane", n_qubits=2)
 
 
 class TestBaseFeatureMap:
     def test__init__(self, base_feature_map: BaseFeatureMap) -> None:
+        assert base_feature_map.platform == "pennylane"
         assert base_feature_map.n_qubits == 2
 
     def test_feature_map(self, base_feature_map: BaseFeatureMap) -> None:
@@ -33,3 +35,7 @@ class TestBaseFeatureMap:
 
     def test_print_circuit(self, base_feature_map: BaseFeatureMap) -> None:
         base_feature_map.print_circuit()
+
+        with pytest.raises(NotImplementedError):
+            base_feature_map.platform = "unsupported"
+            base_feature_map.print_circuit()
