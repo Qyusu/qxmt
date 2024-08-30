c42ccd41d098ab7f860b2113ab43faa859b834e8
kenya-sk <30319295+kenya-sk@users.noreply.github.com>
2024-08-30 22:19:31 +0900

    (add): test for pennylane device

--- tests/utils/test_device.py
+++ tests/utils/test_device.py
@@ -7,9 +7,20 @@ from qxmt.utils import get_number_of_qubits, get_platform_from_device
 
 class TestGetPlatformFromDevice:
     def test_get_platform_from_device_pennylane(self) -> None:
-        device = qml.device("default.qubit", wires=1)
-        platform = get_platform_from_device(device)
-        assert platform == "pennylane"
+        qml_device_list = [
+            qml.device("default.qubit", wires=1),
+            qml.device("default.mixed", wires=1),
+            qml.device("lightning.qubit", wires=1),
+            qml.device("default.qutrit", wires=1),
+            qml.device("default.qutrit.mixed", wires=1),
+            qml.device("default.gaussian", wires=1),
+            # qml.device("default.clifford", wires=1), # need to install stim
+            # qml.device("default.tensor", wires=1),  # need to install quimb
+        ]
+
+        for device in qml_device_list:
+            platform = get_platform_from_device(device)
+            assert platform == "pennylane"
 
     def test_get_platform_from_device_not_supported(self) -> None:
         device = "unsupported_device"
