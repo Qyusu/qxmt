96db2738e05233a580723b852205067f4d4e4d14
kenya-sk <30319295+kenya-sk@users.noreply.github.com>
2024-09-24 13:35:36 +0900

    (docs): add docstrring for device

--- qxmt/devices/builder.py
+++ qxmt/devices/builder.py
@@ -3,10 +3,30 @@ from qxmt.devices.base import BaseDevice
 
 
 class DeviceBuilder:
+    """
+    Builder class for quantum devices.
+
+    Examples:
+        >>> from qxmt.configs import DeviceConfig
+        >>> from qxmt.devices.builder import DeviceBuilder
+        >>> config = DeviceConfig(
+        ...     platform="pennylane",
+        ...     name="default.qubit",
+        ...     n_qubits=2,
+        ...     shots=1000,
+        ... )
+        >>> device = DeviceBuilder(config).build()
+    """
+
     def __init__(
         self,
         config: DeviceConfig,
     ) -> None:
+        """Initialize the device builder.
+
+        Args:
+            config (DeviceConfig): Configuration for the quantum device. It is element of the ExperimentConfig.
+        """
         self.config: DeviceConfig = config
 
     def build(self) -> BaseDevice:
