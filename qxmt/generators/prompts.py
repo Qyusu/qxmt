17efa28ee00d078800b4c476e591ae2320c0bccb
kenya-sk <30319295+kenya-sk@users.noreply.github.com>
2024-09-30 22:19:16 +0900

    (add): role for generator

--- qxmt/generators/prompts.py
+++ qxmt/generators/prompts.py
@@ -2,11 +2,20 @@ from textwrap import dedent
 
 diff_desc_system_prompt = dedent(
     """
-    Please generate one or two sentence discriotion for input diff code.
-    Input diff code contains two parts: ADD CODE and REMOVE CODE.
-    The description should include a clear and concise description of the code change.
+    # Role:
+    You are a code analysis assistant specialized in summarizing GitHub commit changes.
+    Your task is to analyze the given code edits and provide a concise summary of the modifications.
+    Focus on describing what was changed, added, removed, or refactored, including the purpose and impact of these
+    changes. Provide the summary in clear, professional English, highlighting key changes that are relevant to
+    developers reviewing the commit.
+
+    # Task:
+    Please generate one or two sentences as a description for the input diff code.
+    The input diff code contains two parts: ADD CODE and REMOVE CODE.
+    The description should include a clear and concise summary of the code changes.
     """
 )
+
 
 diff_desc_user_prompt = dedent(
     """
