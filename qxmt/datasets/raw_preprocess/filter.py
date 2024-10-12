7f6f019990493cd0c46ec4641b6da538d1077040
kenya-sk <30319295+kenya-sk@users.noreply.github.com>
2024-10-12 23:36:31 +0900

    (chore): fix random seed

--- qxmt/datasets/raw_preprocess/filter.py
+++ qxmt/datasets/raw_preprocess/filter.py
@@ -3,14 +3,17 @@ import numpy as np
 from qxmt.types import RAW_DATASET_TYPE
 
 
-def filter_by_categorical(X: np.ndarray, y: np.ndarray, drop_na: bool = True, n_samples: int = 100) -> RAW_DATASET_TYPE:
+def filter_by_categorical(
+    X: np.ndarray, y: np.ndarray, n_samples: int, random_seed: int, drop_na: bool = True
+) -> RAW_DATASET_TYPE:
     """Filter the dataset by categorical features.
 
     Args:
         X (np.ndarray): input data
         y (np.ndarray): label of input data
+        n_samples (int): number of samples to be extracted
+        random_seed (int): random seed
         drop_na (bool, optional): drop NaN values. Defaults to True.
-        n_samples (int, optional): number of samples to select. Defaults to 100.
 
     Returns:
         RAW_DATASET_TYPE: filtered data
@@ -32,6 +35,7 @@ def filter_by_categorical(X: np.ndarray, y: np.ndarray, drop_na: bool = True, n_
         X_filtered = X_filtered[mask]
         y_filtered = y[mask]
 
-    indices = np.random.choice(X_filtered.shape[0], n_samples, replace=False)
+    rng = np.random.default_rng(random_seed)
+    indices = rng.choice(X_filtered.shape[0], n_samples, replace=False)
 
     return X_filtered[indices], y_filtered[indices]
