87c46815f32092ea9782961f7a6b11e0e988607e
kenya-sk <30319295+kenya-sk@users.noreply.github.com>
2024-11-09 21:13:43 +0900

    (change): add circuit on docstring

--- qxmt/feature_maps/pennylane/ising.py
+++ qxmt/feature_maps/pennylane/ising.py
@@ -13,9 +13,11 @@ class XXFeatureMap(BaseFeatureMap):
 
     Example:
         >>> import numpy as np
-        >>> from qxmt.feature_maps.pennylane.defaults import XXFeatureMap
+        >>> from qxmt.feature_maps.pennylane.ising import XXFeatureMap
         >>> feature_map = XXFeatureMap(2, 2)
-        >>> feature_map(np.random.rand(1, 2))
+        >>> feature_map.draw(x_dim=2)
+        0: ──RX(0.33)─╭IsingXX(15.19)──RX(0.33)─╭IsingXX(15.19)─┤
+        1: ──RX(0.44)─╰IsingXX(15.19)──RX(0.44)─╰IsingXX(15.19)─┤
     """
 
     def __init__(self, n_qubits: int, reps: int) -> None:
@@ -49,9 +51,11 @@ class YYFeatureMap(BaseFeatureMap):
 
     Example:
         >>> import numpy as np
-        >>> from qxmt.feature_maps.pennylane.defaults import YYFeatureMap
+        >>> from qxmt.feature_maps.pennylane.ising import YYFeatureMap
         >>> feature_map = YYFeatureMap(2, 2)
-        >>> feature_map(np.random.rand(1, 2))
+        >>> feature_map.draw(x_dim=2)
+        0: ──H──RY(0.86)─╭IsingYY(11.23)──H──RY(0.86)─╭IsingYY(11.23)─┤
+        1: ──H──RY(0.68)─╰IsingYY(11.23)──H──RY(0.68)─╰IsingYY(11.23)─┤
     """
 
     def __init__(self, n_qubits: int, reps: int) -> None:
@@ -86,9 +90,11 @@ class ZZFeatureMap(BaseFeatureMap):
 
     Example:
         >>> import numpy as np
-        >>> from qxmt.feature_maps.pennylane.defaults import ZZFeatureMap
+        >>> from qxmt.feature_maps.pennylane.ising import ZZFeatureMap
         >>> feature_map = ZZFeatureMap(2, 2)
-        >>> feature_map(np.random.rand(1, 2))
+        >>> feature_map.draw(x_dim=2)
+        0: ──H──RZ(0.88)─╭IsingZZ(13.28)──H──RZ(0.88)─╭IsingZZ(13.28)─┤
+        1: ──H──RZ(0.20)─╰IsingZZ(13.28)──H──RZ(0.20)─╰IsingZZ(13.28)─┤
     """
 
     def __init__(self, n_qubits: int, reps: int) -> None:
