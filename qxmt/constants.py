b8ac0fbb49e9fe9d3b6f1f88476af7eafb4210ba
kenya-sk <30319295+kenya-sk@users.noreply.github.com>
2025-01-15 20:14:17 +0900

    add: alias for environment variable

--- qxmt/constants.py
+++ qxmt/constants.py
@@ -28,7 +28,7 @@ DEFAULT_EXP_DB_FILE: Path = Path("experiment.json")
 # set default config file name.
 DEFAULT_EXP_CONFIG_FILE: Path = Path("config.yaml")
 
-# set environment variable name alias
+# set alias for environment variable name
 IBMQ_API_KEY: str = "IBMQ_API_KEY"
 AWS_ACCESS_KEY_ID: str = "AWS_ACCESS_KEY_ID"
 AWS_SECRET_ACCESS_KEY: str = "AWS_SECRET_ACCESS_KEY"
