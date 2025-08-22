c6395c472178f12cc0792476b77e667259d1ad6d
kenya-sk <30319295+kenya-sk@users.noreply.github.com>
2025-08-22 19:08:11 +0900

    add: particle template

--- qxmt/ansatze/pennylane/__init__.py
+++ qxmt/ansatze/pennylane/__init__.py
@@ -1,6 +1,15 @@
 from qxmt.ansatze.pennylane.all_singles_doubles import AllSinglesDoublesAnsatz
 from qxmt.ansatze.pennylane.gete_fabric import GeteFabricAnsatz
 from qxmt.ansatze.pennylane.kupccgsd import KUpCCGSDAnsatz
+from qxmt.ansatze.pennylane.particle_conserving_u1 import ParticleConservingU1Ansatz
+from qxmt.ansatze.pennylane.particle_conserving_u2 import ParticleConservingU2Ansatz
 from qxmt.ansatze.pennylane.uccsd import UCCSDAnsatz
 
-__all__ = ["AllSinglesDoublesAnsatz", "GeteFabricAnsatz", "KUpCCGSDAnsatz", "UCCSDAnsatz"]
+__all__ = [
+    "AllSinglesDoublesAnsatz",
+    "GeteFabricAnsatz",
+    "KUpCCGSDAnsatz",
+    "ParticleConservingU1Ansatz",
+    "ParticleConservingU2Ansatz",
+    "UCCSDAnsatz",
+]
