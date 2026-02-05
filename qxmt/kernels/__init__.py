3694ae89cd5d935647ac6ce662d68339ef08ad8c
kenya-sk <30319295+kenya-sk@users.noreply.github.com>
2026-02-05 10:49:02 +0900

    add: import qulacs module

--- qxmt/kernels/__init__.py
+++ qxmt/kernels/__init__.py
@@ -1,8 +1,17 @@
 from qxmt.kernels.base import BaseKernel
+from qxmt.kernels.pennylane.base import PennyLaneBaseKernel
+from qxmt.kernels.qulacs.base import QulacsBaseKernel
 from qxmt.kernels.sampling import (
     generate_all_observable_states,
     sample_results_to_probs,
     validate_sampling_values,
 )
 
-__all__ = ["BaseKernel", "generate_all_observable_states", "sample_results_to_probs", "validate_sampling_values"]
+__all__ = [
+    "BaseKernel",
+    "PennyLaneBaseKernel",
+    "QulacsBaseKernel",
+    "generate_all_observable_states",
+    "sample_results_to_probs",
+    "validate_sampling_values",
+]
